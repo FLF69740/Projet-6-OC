a13339acef0c4d212387f4562973f5d768e605a7
francoisLF <francois.lefort_pro@yahoo.com>
2018-07-25 17:04:30 +0200

    firebase auth : facebook account connection

--- app/src/main/java/com/example/francoislf/go4lunch/controllers/activities/MainActivity.java
+++ app/src/main/java/com/example/francoislf/go4lunch/controllers/activities/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.francoislf.go4lunch.controllers.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 import com.example.francoislf.go4lunch.R;
 import com.example.francoislf.go4lunch.controllers.fragments.MainFragment;
 import com.firebase.ui.auth.AuthUI;
-
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import java.util.Arrays;
 import java.util.List;
+import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
 
@@ -29,11 +33,32 @@ public class MainActivity extends BaseActivity {
         this.signInActivity();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK) Toast.makeText(this, R.string.connection_succeed, Toast.LENGTH_SHORT).show();
+            else {
+                if (idpResponse == null) Toast.makeText(this, R.string.connection_canceled, Toast.LENGTH_SHORT).show();
+                else if (idpResponse.getError().equals(ErrorCodes.NO_NETWORK)) Toast.makeText(this, R.string.error_no_internet, Toast.LENGTH_SHORT).show();
+                else Toast.makeText(this, R.string.error_unknown_error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * NAVIGATION
+     */
+
+    // Firebase-UI activity creation
     private void signInActivity(){
-        //I rather using a list, this way handling providers is separeted from the login methods
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
         );
 
         startActivityForResult(
@@ -42,7 +67,10 @@ public class MainActivity extends BaseActivity {
                         .setAvailableProviders(providers)
                         .setTheme(R.style.LoginTheme)
                         .setLogo(R.drawable.logo_firebase_auth)
+                        .setIsSmartLockEnabled(false, true)
                         .build(),
                 RC_SIGN_IN);
     }
+
+
 }
