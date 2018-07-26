d8713e1ee24124f402f46b16fabe68e255aaef71
francoisLF <francois.lefort_pro@yahoo.com>
2018-07-26 16:07:38 +0200

    Navigation drawer creation - icon nav_drawer loading

--- app/src/main/java/com/example/francoislf/go4lunch/controllers/activities/BaseActivity.java
+++ app/src/main/java/com/example/francoislf/go4lunch/controllers/activities/BaseActivity.java
@@ -1,14 +1,25 @@
 package com.example.francoislf.go4lunch.controllers.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+
 import com.example.francoislf.go4lunch.R;
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,6 +27,8 @@ public abstract class BaseActivity extends AppCompatActivity {
         setContentView(this.getContentView());
         configureFragment(savedInstanceState);
         configureToolbar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
     }
 
     // Load the view
@@ -36,11 +49,15 @@ public abstract class BaseActivity extends AppCompatActivity {
     // Load the layout id
     protected abstract int getFragmentLayout();
 
+    /**
+     * Toolbar
+     */
+
     //Configure toolbar
     protected void configureToolbar(){
-        Toolbar toolbar = findViewById(getToolbarView());
-        toolbar.setTitle(getToolbarTitle());
-        setSupportActionBar(toolbar);
+        this.mToolbar = findViewById(getToolbarView());
+        mToolbar.setTitle(getToolbarTitle());
+        setSupportActionBar(mToolbar);
     }
 
     // Load Toolbar View
@@ -52,6 +69,44 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    /**
+     * Navigation drawer
+     */
+
+    protected void configureDrawerLayout(){
+        this.mDrawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    protected void configureNavigationView(){
+        this.mNavigationView = findViewById(R.id.activity_main_nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        else super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.your_lunch :
+                break;
+            case R.id.settings :
+                break;
+            case  R.id.logout :
+                break;
+        }
+
+        this.mDrawerLayout.closeDrawer(GravityCompat.START);
+
         return true;
     }
 }
