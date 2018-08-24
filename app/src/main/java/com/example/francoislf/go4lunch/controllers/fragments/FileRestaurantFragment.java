602f57e72926b0002e895ab29ac4e4c79cdf1758
francoisLF <francois.lefort_pro@yahoo.com>
2018-08-24 16:42:49 +0200

    FileRestaurant Fragment finished in constraintLayout with all widgets

--- app/src/main/java/com/example/francoislf/go4lunch/controllers/fragments/FileRestaurantFragment.java
+++ app/src/main/java/com/example/francoislf/go4lunch/controllers/fragments/FileRestaurantFragment.java
@@ -5,13 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.francoislf.go4lunch.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FileRestaurantFragment extends Fragment {
 
     TextView mRestaurantName;
+    @BindView(R.id.button_restaurant_choice)Button mButtonChoiceRestaurant;
+    @BindView(R.id.restaurant_choice_no_validate)ImageView mImageViewRestaurantChoiceKO;
+    @BindView(R.id.restaurant_choice_validate)ImageView mImageViewRestaurantChoiceOK;
+    Boolean isRestaurantChoosen;
 
     public FileRestaurantFragment(){}
 
@@ -19,11 +30,28 @@ public class FileRestaurantFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_file_restaurant, container, false);
         this.mRestaurantName = view.findViewById(R.id.restaurant_name);
+        ButterKnife.bind(this, view);
+        isRestaurantChoosen = false;
+        setCircleLogoResturantChoice(isRestaurantChoosen);
         return view;
     }
 
     public void setSnippetMarkerName(String name){
         mRestaurantName.setText(name);
+    }
+
+    @OnClick(R.id.button_restaurant_choice)
+    public void changeRestaurantState(){
+        isRestaurantChoosen = !isRestaurantChoosen;
+        setCircleLogoResturantChoice(isRestaurantChoosen);
+    }
+
+    // define the logo of the circle customer restaurant choice
+    private void setCircleLogoResturantChoice(boolean isChoosen){
+        mImageViewRestaurantChoiceKO.setVisibility(View.INVISIBLE);
+        mImageViewRestaurantChoiceOK.setVisibility(View.INVISIBLE);
+        if (isChoosen) mImageViewRestaurantChoiceOK.setVisibility(View.VISIBLE);
+        else mImageViewRestaurantChoiceKO.setVisibility(View.VISIBLE);
     }
 
 }
